825ffd15cbf79bd0f2d41ad19d90ef9cbfe9a71a
Electromayonaise <agazazaca@gmail.com>
2023-12-12 18:57:46 -0500

    Finished predictions
    It is very interesting to see how the model is having trouble to accurately predict the number '5' while working perfectly with the others

--- Main.py
+++ Main.py
@@ -5,84 +5,128 @@ from matplotlib import pyplot as plt
 data = pd.read_csv('data.csv')
 
 data = np.array(data)
-# Rows (m) and features+1 (n)
-m,n = data.shape
+m, n = data.shape
 np.random.shuffle(data)
-data_dev = data[0:1000].T # 1000 random samples for development, and transpose
+
+data_dev = data[0:1000].T
 Y_dev = data_dev[0]
 X_dev = data_dev[1:n]
+X_dev = X_dev / 255.
 
-data_train = data[1000:m].T # Rest for training, and transpose
+data_train = data[1000:m].T
 Y_train = data_train[0]
 X_train = data_train[1:n]
+X_train = X_train / 255.
+_,m_train = X_train.shape
+
 
 def initParameters():
-    W1 = np.random.rand(10, 784)
-    b1 = np.random.randn(10,1)
-    W2 = np.random.rand(10, 10)
-    b2 = np.random.randn(10,1)
+    W1 = np.random.rand(10, 784) - 0.5
+    b1 = np.random.rand(10, 1) - 0.5
+    W2 = np.random.rand(10, 10) - 0.5
+    b2 = np.random.rand(10, 1) - 0.5
     return W1, b1, W2, b2
+
+
+def ReLU(Z):
+    return np.maximum(Z, 0)
+
+
+def softmax(Z):
+    A = np.exp(Z) / sum(np.exp(Z))
+    return A
+
 
 def forwardPropagation(W1, b1, W2, b2, X):
     Z1 = W1.dot(X) + b1
-    A1 = np.maximum(Z1, 0) # ReLU
+    A1 = ReLU(Z1)
     Z2 = W2.dot(A1) + b2
     A2 = softmax(Z2)
     return Z1, A1, Z2, A2
 
-def softmax(Z):
-    return np.exp(Z) / np.sum(np.exp(Z), axis=0)
+
+def ReLU_deriv(Z):
+    return Z > 0
+
+
+def one_hot(Y):
+    one_hot_Y = np.zeros((Y.size, Y.max() + 1))
+    one_hot_Y[np.arange(Y.size), Y] = 1
+    one_hot_Y = one_hot_Y.T
+    return one_hot_Y
+
 
 def backwardPropagation(Z1, A1, Z2, A2, W1, W2, X, Y):
-    m = Y.shape[0]
-    dZ2 = A2 - Y
-    dW2 = 1/m * dZ2.dot(A1.T)
-    db2 = 1/m * np.sum(dZ2)
-    dZ1 = W2.T.dot(dZ2) * (Z1 > 0)
-    dW1 = 1/m * dZ1.dot(X.T)
-    db1 = 1/m * np.sum(dZ1)
+    one_hot_Y = one_hot(Y)
+    dZ2 = A2 - one_hot_Y
+    dW2 = 1 / m * dZ2.dot(A1.T)
+    db2 = 1 / m * np.sum(dZ2)
+    dZ1 = W2.T.dot(dZ2) * ReLU_deriv(Z1)
+    dW1 = 1 / m * dZ1.dot(X.T)
+    db1 = 1 / m * np.sum(dZ1)
     return dW1, db1, dW2, db2
 
-def updateParameters(W1, b1, W2, b2, dW1, db1, dW2, db2, learningRate):
-    W1 = W1 - learningRate * dW1
-    b1 = b1 - learningRate * db1
-    W2 = W2 - learningRate * dW2
-    b2 = b2 - learningRate * db2
+
+def updateParameters(W1, b1, W2, b2, dW1, db1, dW2, db2, alpha):
+    W1 = W1 - alpha * dW1
+    b1 = b1 - alpha * db1
+    W2 = W2 - alpha * dW2
+    b2 = b2 - alpha * db2
     return W1, b1, W2, b2
 
-def gradientDescent(X, Y, learningRate, iterations):
-    W1, b1, W2, b2 = initParameters()
-    for i in range(iterations):
-        Z1, A1, Z2, A2 = forwardPropagation(W1, b1, W2, b2, X)
-        dW1, db1, dW2, db2 = backwardPropagation(Z1, A1, Z2, A2, W1, W2, X, Y)
-        W1, b1, W2, b2 = updateParameters(W1, b1, W2, b2, dW1, db1, dW2, db2, learningRate)
-        if i % 50 == 0:
-            print("Iteration: ", i)
-            print("Accuracy: ", accuracy(A2, Y))
-    return W1, b1, W2, b2
+def get_predictions(A2):
+    return np.argmax(A2, 0)
 
-def accuracy(predictions, Y):
+def get_accuracy(predictions, Y):
     print(predictions, Y)
     return np.sum(predictions == Y) / Y.size
 
-def predict(W1, b1, W2, b2, X):
-    Z1, A1, Z2, A2 = forwardPropagation(W1, b1, W2, b2, X)
-    return np.argmax(A2, axis=0)
+def gradient_descent(X, Y, alpha, iterations):
+    W1, b1, W2, b2 = initParameters()
+    costs = []
+    for i in range(iterations):
+        Z1, A1, Z2, A2 = forwardPropagation(W1, b1, W2, b2, X)
+        dW1, db1, dW2, db2 = backwardPropagation(Z1, A1, Z2, A2, W1, W2, X, Y)
+        W1, b1, W2, b2 = updateParameters(W1, b1, W2, b2, dW1, db1, dW2, db2, alpha)
+        cost = np.sum(-one_hot(Y) * np.log(A2))
+        costs.append(cost)
+        if i % 10 == 0:
+            print("Iteration: ", i)
+            predictions = get_predictions(A2)
+            print(get_accuracy(predictions, Y))
+    plotCosts(costs, alpha, iterations)
+    return W1, b1, W2, b2
 
-def plotCosts(costs, learningRate, iterations):
+def plotCosts(costs, learning_rate, iterations):
     plt.plot(costs)
     plt.ylabel('cost')
-    plt.xlabel('iterations (per tens)')
-    plt.title("Learning rate = " + str(learningRate))
+    plt.xlabel('iterations (per hundreds)')
+    plt.title("Learning rate =" + str(learning_rate))
     plt.show()
 
-def plotPredictions(X, Y, W1, b1, W2, b2):
-    n = X.shape[1]
-    predictions = predict(W1, b1, W2, b2, X)
-    for i in range(n):
-        plt.imshow(X[:,i].reshape(28,28), cmap='gray')
-        plt.title("Prediction: " + str(predictions[i]) + ", Label: " + str(np.argmax(Y[:,i])))
-        plt.show()
+W1, b1, W2, b2 = gradient_descent(X_train, Y_train, 0.10, 500)
 
-W1, b1, W2, b2 = gradientDescent(X_train, Y_train, 0.1, 1000)
+def makePredictions(W1, b1, W2, b2, X, Y):
+    Z1, A1, Z2, A2 = forwardPropagation(W1, b1, W2, b2, X)
+    predictions = get_predictions(A2)
+    return predictions
+def testPrediction(index):
+    X = X_train[:, index, None]
+    Y = Y_train[index]
+    predictions = makePredictions(W1, b1, W2, b2, X, Y)
+    print("Prediction: ", predictions)
+    print("Label: ", Y)
+    X = X.reshape(28, 28)
+    plt.imshow(X, cmap='gray')
+    plt.show()
 
+testPrediction(0)
+testPrediction(1)
+testPrediction(2)
+testPrediction(3)
+testPrediction(4)
+testPrediction(5)
+testPrediction(6)
+testPrediction(7)
+testPrediction(8)
+testPrediction(9)
